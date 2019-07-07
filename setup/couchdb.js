d11528ff1e02f3a6da4275f2e5ea94376447babf
Javier Cordero <javiercorderoperez@gmail.com>
2019-07-07 08:26:03 -0400

    Updated npm packages

--- setup/couchdb.js
+++ setup/couchdb.js
@@ -18,5 +18,5 @@
 
 var nano = require('nano');
 //construct a database wrapper that points to the CouchDB server specified by the URL
-module.exports = nano(process.env.COUCHDB_URL ||'http://admin:t~Y6C_k5]dWM@localhost:5984/');
+module.exports = nano(process.env.COUCHDB_URL ||'http://admin:3*tg1Q!v,>i8|t~5@localhost:5984/');
 console.log(module.exports);
